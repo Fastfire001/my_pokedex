aace4b52764c6de774793a43f2a5473d2904778c
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-24 22:08:27 +0100

    Added function main

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
+function main(data) {
+    document.forms['search'].onsubmit = function () {
+        var idOrName = this.elements['id-name'].value;
+        if (true === isNaN(idOrName)){
+            idOrName = idOrName.substr(0,1).toUpperCase()+ idOrName.substr(1, idOrName.length);
+            for (i in data){
+                if (data[i].name === idOrName){
+                    display(data[i]);
+                    return false
+                }
+            }
+            document.querySelector('div.error').innerHTML = idOrName + ' not found';
+        } else if (false === isNaN(idOrName)){
+            for (i in data){
+                if (i === idOrName){
+                    display(data[i]);
+                    return false
+                }
+            }
+            document.querySelector('div.error').innerHTML = 'Pokémon number ' + idOrName + ' not found';
+        }
+        return false;
+    };
+}
 var request = new XMLHttpRequest();
+var data = {};
 request.open('GET', 'pokemons.json', true);
 request.onload = function() {
     if (this.status >= 200 && this.status < 400) {
-        var data = this.response;
-
+        data = JSON.parse(this.response);
+        window.onload = main(data);
     } else {
         console.log('error');
     }
