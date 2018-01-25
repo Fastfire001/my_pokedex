ea299f2232c970d91173c6f7b304f4e7bdb6cda1
Maxime Maréchal <maxime.marechal@supinternet.fr>
2018-01-25 10:14:54 +0100

    Imporved indentation

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function display(data) {
     document.querySelector('div.pokename').innerHTML = 'Name: <br>' + data.name;
     document.querySelector('div.poketype').innerHTML = 'Type <br>' + data.type;
-    var link = 'http://img.pokemondb.net/artwork/' + data.name.substr(0,data.name.length).toLowerCase() + '.jpg';
+    var link = 'http://img.pokemondb.net/artwork/' + data.name.substr(0, data.name.length).toLowerCase() + '.jpg';
     var img = document.createElement('img');
     img.setAttribute('src', link);
     document.querySelector('div.screen-border').appendChild(img);
@@ -9,18 +9,18 @@ function display(data) {
 function main(data) {
     document.forms['search'].onsubmit = function () {
         var idOrName = this.elements['id-name'].value;
-        if (true === isNaN(idOrName)){
-            idOrName = idOrName.substr(0,1).toUpperCase()+ idOrName.substr(1, idOrName.length);
-            for (i in data){
-                if (data[i].name === idOrName){
+        if (true === isNaN(idOrName)) {
+            idOrName = idOrName.substr(0, 1).toUpperCase() + idOrName.substr(1, idOrName.length);
+            for (i in data) {
+                if (data[i].name === idOrName) {
                     display(data[i]);
                     return false
                 }
             }
             document.querySelector('div.error').innerHTML = idOrName + ' not found';
-        } else if (false === isNaN(idOrName)){
-            for (i in data){
-                if (i === idOrName){
+        } else if (false === isNaN(idOrName)) {
+            for (i in data) {
+                if (i === idOrName) {
                     display(data[i]);
                     return false
                 }
@@ -33,7 +33,7 @@ function main(data) {
 var request = new XMLHttpRequest();
 var data = {};
 request.open('GET', 'pokemons.json', true);
-request.onload = function() {
+request.onload = function () {
     if (this.status >= 200 && this.status < 400) {
         data = JSON.parse(this.response);
         window.onload = main(data);
